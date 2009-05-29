207d1a3409c69dc0f219ae4a87987c8f727bbcea
thelonious <kevin@kevlindev.com>
2009-05-29 10:10:12 -0500

    Minor update to create_docs.sh script

--- com.aptana.sdocgen.html/samples/create_docs.sh
+++ com.aptana.sdocgen.html/samples/create_docs.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-rm -rf ${1}_docs
-mkdir ${1}_docs
-cp $1 ${1}_docs
+mkdir -p ${1}_docs
 cd ${1}_docs
 java -jar ../../libs/saxon8.jar \
-    $1 \
+    ../$1 \
     ../../html/generate_documentation.xslt \
     ReferenceName=SDocGen \
     ReferenceDisplayName="SDocGen Test"
